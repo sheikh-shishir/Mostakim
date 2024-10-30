a886a091abea02c2e233dfffe1ed77a915dd0e06
sheikh-shishir <ahmed2305101594@diu.edu.bd>
2024-10-30 22:02:50 +0600

    first commit

---
+++ src/main/java/com/mycompany/mostakim/Mostakim.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mostakim;
+
+/**
+ *
+ * @author macbookair
+ */
+public class Mostakim {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
